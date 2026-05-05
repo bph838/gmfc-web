68dbac4392a95b4d182ee8a50769b2803b57b65c
BP Hoops <benjaminpaulhooper@gmail.com>
2026-05-05 12:21:42 +0100

    added web page rules framework

---
+++ src/pages/club/rules/general.js
@@ -0,0 +1,27 @@
+import { setupMenuCommands } from "@components/menu";
+import { renderHero } from "@components/hero";
+import { renderSection } from "@components/section";
+import { createDiv, fetchContextArea, renderFinish } from "@framework/dom";
+
+import data from "@data/pages/club/rules/general.json";
+import menu from "@data/generated/menu.json";
+
+setupMenuCommands("page-clubrules",menu);
+renderClubRules(data);
+renderFinish();
+
+function renderClubRules(data) {
+  console.log(data);
+  if (data.content.hero) renderHero(data.content.hero);
+
+  const contentarea = fetchContextArea(data);
+  if (!contentarea) return;
+  const sectionsdiv = createDiv(contentarea, "sections");
+
+  if (data.content.sections) {
+    data.content.sections.forEach((section) => {
+      console.log(section);
+      renderSection(sectionsdiv, section);
+    });
+  }
+}
